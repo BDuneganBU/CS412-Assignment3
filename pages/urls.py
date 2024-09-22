0a7a8764700010974fbff654662bac8feefa777a
BDuneganBU <bdunegan@bu.edu>
2024-09-22 11:42:17 -0400

    Delete pages directory

--- pages/urls.py
+++
@@ -1,9 +0,0 @@
-######## pages/urls.py ########
-# description: direct URL requests to views
-
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', HomePageView),
-] 
--- pages/views.py
+++
@@ -1,8 +0,0 @@
-######## file: pages/views.py ########
-# description: provide a view to send to the user
-
-from django.shortcuts import render
-
-# Create your views here.
-def HomePageView(request):
-    return render(request, "pages/home.html")
